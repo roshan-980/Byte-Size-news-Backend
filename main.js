abeea6a94cd4a170ced37d46ba6c102aa7873155
roshan-980 <playerpro9800@gmail.com>
2026-04-27 00:07:51 +0530

    updated the listening part

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 
 })
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
 
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+});
