be0bfb473f75752949f9f205fb63ff4e98f72ed0
roshan-980 <playerpro9800@gmail.com>
2026-04-26 23:52:41 +0530

    updated the port again and again

--- main.js
+++ main.js
@@ -39,8 +39,8 @@ app.use('/save', savefeatureroute);
 app.get('/', (req, res) => {
 
 })
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT || 5000}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
 
